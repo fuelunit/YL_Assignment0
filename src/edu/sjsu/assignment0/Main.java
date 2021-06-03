b0f465f57296d1671de1f8c2973d871e7f691b4d
fuelunit <yipeng.liu@sjsu.edu>
2021-06-03 15:00:03 -0700

    Initial commit

---
+++ src/edu/sjsu/assignment0/Main.java
@@ -0,0 +1,11 @@
+package edu.sjsu.assignment0;
+
+/**
+ * Author: Yipeng Liu 014057498
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello CS49J!");
+    }
+}
